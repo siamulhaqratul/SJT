aeb1f40de7ed13a113b5a9754aca81a811554a92
Siamul Haq Ratul <siamulhaqratul@gmail.com>
2022-06-02 17:32:56 +0600

    Add files via upload

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["sprPlayer","sprEnemy"],collisionMatrix:[[false,true],[true,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/GamePlay.fire",orientation:"",jsList:[]};
